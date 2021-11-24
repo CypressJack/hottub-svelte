b84be5df9e3267d10640fe24db345334543f0bb1
wade <wadecroft@gmail.com>
2021-11-23 19:44:06 -0800

    refactor

--- control/hottub.js
+++ control/hottub.js
@@ -4,72 +4,82 @@ const app = express();
 const cors = require('cors');
 const port = 3000;
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 3005 });
-
-let setPoint = 78;
-let jets = false;
-let status;
-
-wss.on('connection', function connection(ws) {
-    const sendData = JSON.stringify({
-		currentTemp: 65,
-		setPoint: 78,
-	});
-    ws.send(sendData);
-    ws.on('message', function incoming(message) {
-        const res = JSON.parse(message);
-        console.log(`received message:`, res);
-        if (res === 'setPointDown') {
-            setPoint--;
-            ws.send(JSON.stringify({'setPoint': setPoint}));
-        }
-
-        if (res === 'setPointUp') {
-            setPoint++;
-            ws.send(JSON.stringify({'setPoint': setPoint}));
-        }
-
-        if (res === 'jetsOn') {
-            jets = true;
-        }
-
-        if (res === 'jetsoff') {
-            jets = false;
-        }
-    });
-    ws.send(JSON.stringify(`message received from Node!`));
-
-    setInterval(() => {
-        const curTempC = ds18b20.temperatureSync(sensorId);
-        const curTempF = ( curTempC * (9/5) ) + 32;
-        ws.send(JSON.stringify({'currentTemp': curTempF}));
-    }, 250);
-
-});
 
 // Controller config
 const rpio = require('rpio');
 const ds18b20 = require('ds18b20');
 const { Timer } = require("easytimer.js");
 
-// Control logic
+// Initialize websocket
+const wss = new WebSocket.Server({ port: 3005 });
+
+// Control
 const sensorId = '28-011937c40830';
 const runningTime = new Timer();
 const jetTimer = new Timer();
+let setPoint = 78;
+let jets = false;
+let status;
+
+// Websocket Connection
+wss.on('connection', function connection(ws) {
+    const sendData = JSON.stringify({
+		currentTemp: null,
+		setPoint: null,
+	});
+    ws.send(sendData);
+    ws.on('message', function incoming(message) {
+        const res = JSON.parse(message);
+        console.log(`received message:`, res);
+
+        // Lower Setpoint
+        if (res === 'setPointDown') {
+            setPoint--;
+            ws.send(JSON.stringify({'setPoint': setPoint}));
+        }
+        // Raise Setpoint
+        if (res === 'setPointUp') {
+            setPoint++;
+            ws.send(JSON.stringify({'setPoint': setPoint}));
+        }
+        // Turn jets on
+        if (res === 'jetsOn') {
+            jets = true;
+        }
+        // Turn jets off
+        if (res === 'jetsoff') {
+            jets = false;
+        }
+    });
+    ws.send(JSON.stringify(`message received from Node!`));
+
+    const tempStream = setInterval(() => {
+        ds18b20.temperature('10-00080283a977', function(err, value) {
+            const curTempF = ( value * (9/5) ) + 32;
+            ws.send(JSON.stringify({'currentTemp': curTempF}));
+        });
+    }, 250);
+
+    // Handle websocket close
+    ws.on('close', function close() {
+        clearInterval(tempStream);
+        console.log('disconnected');
+      });
+});
 
 // Initialize GPIO pin as low
 rpio.open(11, rpio.OUTPUT, rpio.LOW);
+rpio.open(12, rpio.OUTPUT, rpio.LOW);
+rpio.write(12, rpio.HIGH);
 
+// monitor temp and turn on and off motor
 const tempLoop = setInterval(manageTemp, 1000);
 
 function manageTemp() {
     const curTempC = ds18b20.temperatureSync(sensorId);
     const curTempF = ( curTempC * (9/5) ) + 32;
 
-    if (jets) {
-        jetTimer.start();
-    }
-
+    
     // Check to make sure temp isn't too high & turn off
     if (curTempF > 105) {
         rpio.write(11, rpio.LOW);
@@ -77,19 +87,23 @@ function manageTemp() {
         console.log('Tub Emergency Shutdown');
         runningTime.reset();
     }
-
+    
     // Check if temp is higher than setpoint & turn off
-    if (curTempF > setPoint && !jets) {
+    if (curTempF > setPoint) {
         rpio.write(11, rpio.LOW);
         status = rpio.read(11);
         runningTime.reset();
     }
-
+    
     // Check if temp is lower than set point & turn on
-    if ((curTempF < setPoint && rpio.read(11) === 0) || jets ) {
+    if (curTempF < setPoint && rpio.read(11) === 0) {
         rpio.write(11, rpio.HIGH);
         status = rpio.read(11);
         runningTime.start();
+    }
+
+    if (jets) {
+        jetTimer.start();
     }
 
     if (jetTimer.minutes < 60 && jets) {
@@ -101,11 +115,6 @@ function manageTemp() {
     }
 
 }
-
-
-
-// Express Server
-app.use(cors());
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
